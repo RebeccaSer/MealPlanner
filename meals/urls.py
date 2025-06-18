ff75c6440ca6ff89cdb9f0d38e66adc43489c495
seserepjhb024 <seserepjhb024@student.wethinkcode.co.za>
2025-06-18 22:52:42 +0200

    Added views

--- meals/urls.py
+++ meals/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('prep/', views.meal_prep, name='prep'),
+    path('grocery/', views.grocery_list, name='grocery'),
+    path('quick/', views.quick_meals, name='quick'),
+    path('bakes/', views.bakes, name='bakes'),
+    path('lunch/', views.lunch, name='lunch'),
+    path('desserts/', views.desserts, name='desserts'),
+    path('about/', views.about_us, name='about')
 ]
--- meals/views.py
+++ meals/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'meals/home.html')
 
+def meal_prep(request):
+    return render(request, 'meals/prep.html')
+
+def grocery_list(request):
+    return render(request, 'meals/grocery.html')
+
+def quick_meals(request):
+    return render(request, 'meals/quick.html')
+
+def bakes(request):
+    return render(request, 'meals/bakes.html')
+
+def lunch(request):
+    return render(request, 'meals/lunch.html')
+
+def desserts(request):
+    return render(request, 'meals/desserts.html')
+
+def about_us(request):
+    return render(request, 'meals/about.html')
